c1c38ff95762060b3e795a46c1ec6874fca90a00
ArchieLennon <78304183+ArchieLennon@users.noreply.github.com>
2025-11-07 15:37:56 +0000

    added force dynamic so randomiser works

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,37 @@
+export const dynamic = "force-dynamic";
 
-import Title from "./components/Title"
+import Title from "./components/Title";
 import ImageWrapper from "./components/ImageWrapper";
 import { getProjects } from "@/sanity/queries/sanityQueriy";
 
-
 interface randomProjectProps {
-    
-    title: string;
+  title: string;
   backgroundColour: { hex: string };
   textColour: { hex: string };
-    imageUrl: string;
+  imageUrl: string;
 }
-
 
 export default async function Home() {
-
   const projects = await getProjects();
-  const randomProject:randomProjectProps = projects[Math.floor(Math.random() * projects.length)];
+  const randomProject: randomProjectProps =
+    projects[Math.floor(Math.random() * projects.length)];
 
-  
   return (
-    <div 
-    style={
-  randomProject.backgroundColour?.hex
-    ? { backgroundColor: randomProject.backgroundColour.hex }
-    : {}
-}
-    className="flex w-screen h-screen   font-sans dark:bg-black">
-        <div className="w-full h-full absolute pointer-events-none z-0 ">
-          <ImageWrapper image={randomProject}/>
-        </div>
+    <div
+      style={
+        randomProject.backgroundColour?.hex
+          ? { backgroundColor: randomProject.backgroundColour.hex }
+          : {}
+      }
+      className="flex w-screen h-screen font-sans dark:bg-black"
+    >
+      <div className="w-full h-full absolute pointer-events-none z-0">
+        <ImageWrapper image={randomProject} />
+      </div>
 
       <div className="p-5 md:p-5 lg:p-10 z-10">
-          <Title textCol={randomProject}/>
-       </div>
-
-    
+        <Title textCol={randomProject} />
+      </div>
     </div>
   );
 }
